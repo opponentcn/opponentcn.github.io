9fe7cbb88a38b265f44f0a5060eb145fc1c8bf05
citing <citingnutria@gmail.com>
2021-11-17 17:57:47 +0800

    test

---
+++ manag.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+Date=$(date +'%m%d')
+Date_before=$(date -d"-2 day" +%m%d)
+echo ${Date}
+echo ${Date_before}
+mv ${Date_before}/ ${Date}/
+git add *
+git rm *
+git commit -m"${Date}"
+git push origin main
+
